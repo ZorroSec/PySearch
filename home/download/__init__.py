0d81143233b8a959add698e648ed9e39b2029ad3
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-06 13:34:26 -0300

    main

--- home/download/__init__.py
+++ home/download/__init__.py
@@ -1,8 +1,8 @@
-from pytube import Youtube, streams
+from pytube import YouTube, streams
 from pytube.cli import on_progress
 from colorama import Fore
 
-class YtDonwload:
+class YtDownload:
     def __init__(self, link, yt, ys):
         self.link = link
         self.yt = yt
@@ -12,11 +12,11 @@ class YtDonwload:
         self.ys.download()
     
     def ytdownload(self):
-        print(f"{Fore.GREEN}Title => {Fore.BLACK}{yt.title}")
+        print(f"{Fore.GREEN}Title => {Fore.BLACK}{self.yt.title}")
         print(f"{Fore.GREEN}Downloading...")
-        YtDonwload.download()
 
 link = "https://youtu.be/Z1GNW_woXI0"
-yt = Youtube(link, on_progress_callback=on_progress)
+yt = YouTube(link, on_progress_callback=on_progress)
 ys = yt.streams.get_highest_resolution()
-YtDonwload(link, yt, ys).download()
+YtDownload(link, yt, ys).ytdownload()
+YtDownload(link, yt, ys).download()
