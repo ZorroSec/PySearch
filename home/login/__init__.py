86bc160ef6cf30eaa685659bb79b94a9a3eba1bd
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-02-06 00:00:27 -0300

    main

--- home/login/__init__.py
+++ home/login/__init__.py
@@ -2,8 +2,6 @@ from colorama import Fore
 
 def login():
     username = input(f"{Fore.GREEN}Username > ")
-    if username:
-        print(f"{Fore.LIGHTBLACK_EX} Ola {username}")
-        with open('data.txt', 'w') as file:
-            file.write(username)
-        
+    print(f"{Fore.LIGHTBLACK_EX} Ola {username}")
+    with open('../../data.txt', 'w') as file:
+        file.write(username)
